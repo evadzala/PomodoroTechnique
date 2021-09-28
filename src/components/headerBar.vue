990e1a510d893932b75cf12213bc18a79b0797e0
HLW\peter.chiu <peter.chiu@hlwtech.com.tw>
2021-09-28 10:25:24 +0800

    調整router及headerBar
    (cherry picked from commit ebd823f58667d0653d034d342d970535d38622e5)

---
+++ src/components/headerBar.vue
@@ -0,0 +1,36 @@
+<template lang="pug">
+  .headerBar
+    .outline(@click="goToOtherPage('todoList')") todoList
+    .outline(@click="goToOtherPage('countdownClock')") clock
+
+</template>
+
+<script>
+export default {
+  name: 'headerBar',
+
+  methods: {
+    goToOtherPage (pageName) {
+      this.$router.push(pageName)
+    }
+  }
+}
+</script>
+
+<style>
+.headerBar {
+  margin: 0;
+  height: 56px;
+  padding: 0 16px 0 24px;
+  background-color: #434343;
+	display: flex;
+	justify-content: space-between;
+  align-items: center;
+}
+
+.outline {
+  background-color: #f5a623;
+	outline: 2px solid #f5a623;
+	outline-offset: 2px;
+}
+</style>
