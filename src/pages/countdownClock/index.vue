37a4bf57239d97028ccb052c20d4a0e9e3a5d699
HLW\peter.chiu <peter.chiu@hlwtech.com.tw>
2021-10-08 17:50:51 +0800

    倒數頁面基本功能完成

---
+++ src/pages/countdownClock/index.vue
@@ -0,0 +1,38 @@
+<template lang="pug">
+  .countdown-clock
+    clock
+    
+</template>
+
+<script>
+import clock from './clock.vue'
+
+export default {
+  components: {
+    clock
+  },
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style>
+.outside-circle {
+  width: 200px;
+  height: 200px;
+  position: relative;
+  border-radius: 50%;
+  border: 5px solid black;
+  margin: 100px auto;
+}
+
+.kedu {
+  width: 200px;
+  height: 200px;
+  position: absolute;
+}
+
+
+</style>
