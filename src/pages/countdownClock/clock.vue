5f7754a5e136418801f9a7f1bbb0d6deeeba355a
HLW\peter.chiu <peter.chiu@hlwtech.com.tw>
2021-10-14 16:55:14 +0800

    修改倒數器機制

--- src/pages/countdownClock/clock.vue
+++ src/pages/countdownClock/clock.vue
@@ -1,11 +1,11 @@
 <template lang="pug">
   .container
-    .clock
+    .workClock
       h1 秒 {{ counter }}
-      h1 分:秒 {{ paddingzero(Math.floor(counter/60), 2) }}:{{ paddingzero(counter % 60, 2) }} 
+      h1 分:秒 {{ paddingzero(Math.floor(counter/60), 2) }}:{{ paddingzero(counter % 60, 2) }}
     .control
-      button(v-if="status === 'start'" @click="startCount") start
-      button(v-else-if="status === 'stop'" @click="stopCount") stop
+      button(v-if="['start', 'workTimeFinish', 'restTimeFinish'].includes(status)" @click="startCount") start
+      button(v-else-if="['workTime', 'restTime'].includes(status)" @click="stopCount") stop
       div(v-else)
         button(v-if="counter !== 0" @click="continueCount") continue
         button(@click="restart") restart
@@ -16,12 +16,12 @@ export default {
   data () {
     return {
       // 專注時間與休息時間以秒為單位
-      workCounterSetting: 100,
-      breaktimeCounterSetting: 5,
+      workCounterSetting: 5,
+      restTimeCounterSetting: 2,
       timer: null,
 
       counter: 0,
-      status: 'start'
+      status: null
     }
   },
 
@@ -32,35 +32,49 @@ export default {
   methods: {
     init () {
       // 進行初始化
+      this.status = 'start'
       this.counter = this.workCounterSetting
     },
 
+    // button event
     startCount () {
-      this.status = 'stop'
+      if (['start', 'restTimeFinish'].includes(this.status)) {
+        this.status = 'workTime'
+      } else if (['workTimeFinish'].includes(this.status)) {
+        this.status = 'restTime'
+      }
       this.timer = setInterval(this.countDown, 1000)
     },
     stopCount () {
-      this.status = null
-      if (!this.status) {
-        clearInterval(this.timer)
-      }
+      clearInterval(this.timer)
+
+      if (this.status === 'workTime') this.status = 'workStop'
+      else if (this.status === 'restTime') this.status = 'restStop'
     },
+    continueCount () {
+      this.timer = setInterval(this.countDown, 1000)
+
+      if (this.status === 'workStop') this.status = 'workTime'
+      else if (this.status === 'restStop') this.status = 'restTime'
+    },
+    restart () {
+      this.init()
+    },
+
     countDown () {
       // 把時間減一
       this.counter--
-      if (this.counter === 0) {
-        this.stopCount()
+      if (this.counter === -1) {
+        clearInterval(this.timer)
+
+        if (this.status === 'workTime') {
+          this.status = 'workTimeFinish'
+          this.counter = this.restTimeCounterSetting
+        } else if (this.status === 'restTime') {
+          this.status = 'restTimeFinish'
+          this.counter = this.workCounterSetting
+        }
       }
-    },
-    continueCount () {
-      this.status = 'stop'
-
-      this.timer = setInterval(this.countDown, 1000)
-    },
-    restart () {
-      this.status = 'start'
-
-      this.init()
     },
     paddingzero (num, length) {
       return (Array(length).join('0') + num).slice(-length)
